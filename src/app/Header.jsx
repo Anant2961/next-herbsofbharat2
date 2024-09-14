8400dca667790c04085e5992a8e56751d57f3884
anant2961 <anant2961@gmail.com>
2024-09-14 07:28:48 +0530

    testing functions

---
+++ src/app/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div>
+      <button className="bg-red-300 hover:border-white transform translate-x-12 hover:translate-x-0">
+        click here
+      </button>
+    </div>
+  );
+}
